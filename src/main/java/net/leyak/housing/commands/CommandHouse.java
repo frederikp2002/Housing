a87ca5795a0ef90e4c2b98a2d0c4d833625c546d
Frederik P <frederikp2002@gmail.com>
2023-06-11 16:00:36 +0200

    Refactored the Command class to apply to better coding practices + better readability

--- src/main/java/net/leyak/housing/commands/CommandHouse.java
+++ src/main/java/net/leyak/housing/commands/CommandHouse.java
@@ -2,6 +2,7 @@ package net.leyak.housing.commands;
 
 import net.kyori.adventure.text.Component;
 import net.leyak.housing.Housing;
+import net.leyak.housing.commands.resident.Resident;
 import net.leyak.housing.commands.settings.Settings;
 import net.leyak.housing.handlers.PlayerInteraction;
 import net.leyak.housing.handlers.WorldManagement;
@@ -14,78 +15,94 @@ import org.jetbrains.annotations.NotNull;
 import java.util.List;
 
 public class CommandHouse implements CommandExecutor {
+
     private final Housing plugin;
     private final WorldManagement worldManagement;
     private final PlayerInteraction playerInteraction;
     private final Settings settings;
-    private final CommandReload reload;
+    private final Resident resident;
     private final CommandHelp commandHelp;
+    private final CommandReload reload;
+
+    private static final String RELOAD_COMMAND = "reload";
+    private static final String SPAWN_COMMAND = "spawn";
+    private static final String HELP_COMMAND = "help";
 
     public CommandHouse(Housing plugin) {
         this.plugin = plugin;
         this.worldManagement = new WorldManagement(plugin);
         this.playerInteraction = new PlayerInteraction(plugin);
         this.settings = new Settings(plugin);
-        this.reload = new CommandReload(plugin);
+        this.resident = new Resident(plugin);
         this.commandHelp = new CommandHelp(plugin);
+        this.reload = new CommandReload(plugin);
     }
 
     @Override
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
-        if (!(commandSender instanceof Player)) {
-            plugin.getLogger().warning(plugin.getMessageHandler().getMessage("command.housing.notPlayer").content());
-            return false;
-        }
-
+        if (!(commandSender instanceof Player)) { sendNotPlayerWarning(); return false; }
         if (!command.getName().equalsIgnoreCase("housing")) return false;
 
         Player player = (Player) commandSender;
         String worldName = "housing-" + player.getUniqueId() + "-1";
 
-        List<String> settingsCommands = plugin.getConfigHandler().getStringList("commands.housing.settings.argument");
-
-        // "/housing" command without any arguments
         if (strings.length == 0) {
-            if (plugin.getCore().getMVWorldManager().getMVWorld(worldName) != null) {
-                settings.openSettingsGUI(player);
-                return true;
-            }
-            worldManagement.createNewHousing(player, worldName);
-            return true;
-        }
-        // "/housing <argument>" command
-        else {
-            // "/housing settings" command
-            if (settingsCommands.contains(strings[0])) {
-                settings.openSettingsGUI(player);
-                return true;
-            // "/housing reload" command
-            } else if (strings[0].equalsIgnoreCase("reload")) {
-                reload.reload(player);
-                return true;
-            // "/housing spawn" command
-            } else if (strings[0].equalsIgnoreCase("spawn")) {
-                if (plugin.getCore().getMVWorldManager().getMVWorld(worldName) != null) {
-                    playerInteraction.teleportPlayerToWorld(player, worldName);
-                    return true;
-                }
-                player.sendMessage(plugin.getMessageHandler().getMessage("command.housing.spawn.noHouse"));
-                return true;
-            // "/housing help" command
-            } else if (strings[0].equalsIgnoreCase("commandHelp")) {
-                List<Component> helpMessages = commandHelp.help();
-                for (Component message : helpMessages) {
-                    player.sendMessage(message);
-                }
-                return true;
-            }
-            // "/housing <invalid_argument>" command
-            else {
-                player.sendMessage(plugin.getMessageHandler().getMessage("command.housing.invalidArgument", s, s));
-                return true;
-            }
+            return handleNoArgumentsCommand(player, worldName);
+        } else {
+            return handleCommandWithArguments(player, worldName, strings[0]);
         }
     }
 
+    private void sendNotPlayerWarning() {
+        plugin.getLogger().warning(plugin.getMessageHandler().getMessage("command.housing.notPlayer").content());
+    }
 
+    private boolean handleNoArgumentsCommand(Player player, String worldName) {
+        if (plugin.getCore().getMVWorldManager().getMVWorld(worldName) != null) {
+            settings.openSettingsGUI(player);
+            return true;
+        }
+        worldManagement.createNewHousing(player, worldName);
+        return true;
+    }
+
+    private boolean handleCommandWithArguments(Player player, String worldName, String firstArg) {
+        List<String> settingsCommands = plugin.getConfigHandler().getStringList("commands.housing.settings.argument");
+        List<String> residentCommands = plugin.getConfigHandler().getStringList("commands.housing.resident.argument");
+
+        if (settingsCommands.contains(firstArg.toLowerCase())) {
+            settings.openSettingsGUI(player);
+            return true;
+        } else if (residentCommands.contains(firstArg.toLowerCase())) {
+            resident.openResidentGUI(player);
+            return true;
+        } else if (firstArg.equalsIgnoreCase(RELOAD_COMMAND)) {
+            reload.reload(player);
+            return true;
+        } else if (firstArg.equalsIgnoreCase(SPAWN_COMMAND)) {
+            return handleSpawnCommand(player, worldName);
+        } else if (firstArg.equalsIgnoreCase(HELP_COMMAND)) {
+            return handleHelpCommand(player);
+        } else {
+            player.sendMessage(plugin.getMessageHandler().getMessage("command.housing.invalidArgument", firstArg, firstArg));
+            return true;
+        }
+    }
+
+    private boolean handleSpawnCommand(Player player, String worldName) {
+        if (plugin.getCore().getMVWorldManager().getMVWorld(worldName) != null) {
+            playerInteraction.teleportPlayerToWorld(player, worldName);
+            return true;
+        }
+        player.sendMessage(plugin.getMessageHandler().getMessage("command.housing.spawn.noHouse"));
+        return false;
+    }
+
+    private boolean handleHelpCommand(Player player) {
+        List<Component> helpMessages = commandHelp.help();
+        for (Component message : helpMessages) {
+            player.sendMessage(message);
+        }
+        return true;
+    }
 }
